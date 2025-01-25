24fd8f7bfcc46a20b28de829f5fb1bc627012247
Gabor Krisko <git@krisko.dev>
2025-01-25 21:50:45 +0100

    implement byte string

--- main.ts
+++ main.ts
@@ -1,18 +1,3 @@
-type LiteralEvent = {
-	eventType: "literal",
-	majorType: number;
-	data: number | bigint;
-};
-type CborEvent = LiteralEvent;
-
-export interface Decoder {
-	[Symbol.asyncIterator](): AsyncIterator<CborEvent>;
-}
-
-const Mode = Object.freeze({
-	ExpectingDataItem: -1,
-	MajorType: 0,
-});
 export const MajorType = Object.freeze({
 	UnsignedInteger: 0,
 	NegativeInteger: 1,
@@ -23,6 +8,36 @@ export const MajorType = Object.freeze({
 	Tag: 6,
 	SimpleValue: 7,
 });
+
+export type LiteralEvent = {
+	eventType: "literal",
+	majorType: typeof MajorType["NegativeInteger"] | typeof MajorType["UnsignedInteger"] | typeof MajorType["Tag"];
+	data: number | bigint;
+};
+type StartEvent = {
+	eventType: "start",
+	majorType: typeof MajorType["ByteString"],
+};
+type EndEvent = {
+	eventType: "end",
+	majorType: typeof MajorType["ByteString"],
+};
+type DataEvent = {
+	eventType: "data",
+	majorType: typeof MajorType["ByteString"];
+	data: Uint8Array;
+};
+type CborEvent = LiteralEvent | StartEvent | EndEvent | DataEvent;
+
+export interface Decoder {
+	[Symbol.asyncIterator](): AsyncIterator<CborEvent>;
+}
+
+const Mode = Object.freeze({
+	ExpectingDataItem: -1,
+	MajorType: 0,
+});
+
 const numbericalTypes = [
 	MajorType.UnsignedInteger,
 	MajorType.NegativeInteger,
@@ -33,38 +48,72 @@ function isNumerical(type: number) {
 	return (numbericalTypes as number[]).includes(type);
 }
 
+export async function* consumeByteString(iterator: AsyncIterableIterator<CborEvent,void,void>): AsyncIterableIterator<Uint8Array,void,void> {
+	let counter = 1;
+    while (true) {
+        const { done, value } = await iterator.next();
+        if (done) {
+            break;
+        }
+        if (value.eventType === "start") {
+            counter++;
+        }
+        if (value.eventType === "end") {
+            counter--;
+        }
+        if (counter === 0) {
+            break;
+        }
+        if (value.eventType === "data") {
+			yield value.data;
+        }
+    }
+}
+
 export function decoderFromStream(stream: ReadableStream<Uint8Array>) {
 	const reader = stream.getReader();
 
+	const noYield = Symbol();
+	let currentBuffer = new Uint8Array();
+	let mode: number = Mode.ExpectingDataItem;
+	let index = 0;
+	let majorType: number = NaN;
+	let additionalInfo: number = NaN;
+	let numberOfBytesToRead = 0;
+	let numberValue: number | bigint = 0;
+	let isIndefinite = false;
+	let byteArrayNumberOfBytesToRead = 0;
+	let yieldValue: CborEvent | typeof noYield = noYield;
+
 	return {
 		[Symbol.asyncIterator]() {
-			const noYield = Symbol();
 			return async function* it(): AsyncIterableIterator<CborEvent,void,void> {
-				let currentBuffer = new Uint8Array();
-				let mode: number = Mode.ExpectingDataItem;
-				let index = 0;
-				let majorType: number = NaN;
-				let additionalInfo: number = NaN;
-				let numberOfBytesToRead = 0;
-				let numberOfBytesToReadTotal = 0;
-				let numberValue: number | bigint = 0;
-				let isIndefinite = false;
-
-				let yieldValue: CborEvent | typeof noYield = noYield;
-
 				function flushNumber() {
-					if (majorType === MajorType.NegativeInteger) {
-						if (typeof numberValue === "bigint") {
-							numberValue = (numberValue * -1n) -1n;
-						} else {
-							numberValue = (numberValue * -1) -1;
+					if (isNumerical(majorType)) {
+						mode = Mode.ExpectingDataItem;
+						if (majorType === MajorType.NegativeInteger) {
+							if (typeof numberValue === "bigint") {
+								numberValue = (numberValue * -1n) -1n;
+							} else {
+								numberValue = (numberValue * -1) -1;
+							}
 						}
+						yieldValue = {
+							eventType: "literal",
+							majorType: majorType as any,
+							data: numberValue,
+						};
 					}
-					yieldValue = {
-						eventType: "literal",
-						majorType,
-						data: numberValue,
-					};
+					if (majorType == MajorType.ByteString) {
+						byteArrayNumberOfBytesToRead = Number(numberValue);
+						if (numberValue > Number.MAX_SAFE_INTEGER) {
+							throw new Error("Array too large");
+						}
+						yieldValue = {
+							eventType: "start",
+							majorType: MajorType.ByteString,
+						};
+					}
 				}
 
 				while (true) {
@@ -72,7 +121,7 @@ export function decoderFromStream(stream: ReadableStream<Uint8Array>) {
 						yield yieldValue;
 						yieldValue = noYield;
 					}
-					if (currentBuffer.length-index <= 0) {
+					if (index >= currentBuffer.length) {
 						const { done, value } = await reader.read();
 						if (done) {
 							return;
@@ -80,64 +129,72 @@ export function decoderFromStream(stream: ReadableStream<Uint8Array>) {
 						currentBuffer = value;
 						index = 0;
 					}
-					while (true) {
-						if (index >= currentBuffer.length) {
-							break;
+					if (mode == Mode.MajorType && majorType == MajorType.ByteString) {
+						if (byteArrayNumberOfBytesToRead <= 0) {
+							yieldValue = {
+								eventType: "end",
+								majorType: MajorType.ByteString,
+							}
+							continue;
 						}
-						if (yieldValue != noYield) {
-							yield yieldValue;
-							yieldValue = noYield;
+						const to = index + byteArrayNumberOfBytesToRead;
+						const slice = currentBuffer.slice(index, to);
+						index += byteArrayNumberOfBytesToRead;
+						yieldValue = {
+							eventType: "data",
+							majorType: MajorType.ByteString,
+							data: slice,
 						}
-						const byte = currentBuffer[index];
-						if (mode == Mode.ExpectingDataItem) {
-							majorType = byte >>> 5;
-							mode = Mode.MajorType;
-							additionalInfo = byte & 0b00011111;
-							numberValue = 0;
-							if (additionalInfo < 24) {
-								numberOfBytesToRead = numberOfBytesToReadTotal = 0;
-								numberValue = additionalInfo;
-								flushNumber();
-							}
-							if (additionalInfo == 24) {
-								numberOfBytesToRead = numberOfBytesToReadTotal = 1;
-							}
-							if (additionalInfo == 25) {
-								numberOfBytesToRead = numberOfBytesToReadTotal = 2;
-							}
-							if (additionalInfo == 26) {
-								numberOfBytesToRead = numberOfBytesToReadTotal = 4;
-							}
-							if (additionalInfo == 27) {
-								numberValue = 0n;
-								numberOfBytesToRead = numberOfBytesToReadTotal = 8;
-							}
-							if ([28,29,30].includes(additionalInfo)) {
-								throw new Error("Reserved");
-							}
-							if (additionalInfo == 31) {
-								isIndefinite = true;
-								numberOfBytesToRead = 0;
-							}
-							if (isIndefinite && isNumerical(majorType)) {
-								throw new Error(`Ill-formed Data Item of Major Type ${majorType}`);
-							}
-						} else if (mode == Mode.MajorType && isNumerical(majorType)) {
-							if (typeof numberValue == "bigint") {
-								numberValue = (numberValue << 8n) | BigInt(byte);
-							} else {
-								numberValue = (numberValue << 8) | byte;
-							}
-							
-							numberOfBytesToRead--;
-							if (numberOfBytesToRead == 0) {
-								mode = Mode.ExpectingDataItem;
-								flushNumber();
-							}
-						}
-
-						index++;
+						continue;
 					}
+					const byte = currentBuffer[index];
+					if (mode == Mode.ExpectingDataItem) {
+						majorType = byte >>> 5;
+						mode = Mode.MajorType;
+						additionalInfo = byte & 0b00011111;
+						numberValue = 0;
+						if (additionalInfo < 24) {
+							numberOfBytesToRead = 0;
+							numberValue = additionalInfo;
+							flushNumber();
+						}
+						if (additionalInfo == 24) {
+							numberOfBytesToRead = 1;
+						}
+						if (additionalInfo == 25) {
+							numberOfBytesToRead = 2;
+						}
+						if (additionalInfo == 26) {
+							numberOfBytesToRead = 4;
+						}
+						if (additionalInfo == 27) {
+							numberValue = 0n;
+							numberOfBytesToRead = 8;
+						}
+						if ([28,29,30].includes(additionalInfo)) {
+							throw new Error("Reserved");
+						}
+						if (additionalInfo == 31) {
+							isIndefinite = true;
+							numberOfBytesToRead = 0;
+						}
+						if (isIndefinite && isNumerical(majorType)) {
+							throw new Error(`Ill-formed Data Item of Major Type ${majorType}`);
+						}
+					} else if (mode == Mode.MajorType && numberOfBytesToRead > 0) {
+						if (typeof numberValue == "bigint") {
+							numberValue = (numberValue << 8n) | BigInt(byte);
+						} else {
+							numberValue = (numberValue << 8) | byte;
+						}
+						
+						numberOfBytesToRead--;
+						if (numberOfBytesToRead == 0) {
+							flushNumber();
+						}
+					}
+
+					index++;
 				}
 			}();
 		}
--- main_test.ts
+++ main_test.ts
@@ -1,7 +1,34 @@
 import { assertEquals } from "@std/assert";
-import { decoderFromStream } from "./main.ts";
+import { consumeByteString, decoderFromStream, LiteralEvent } from "./main.ts";
 import { MajorType } from "./main.ts";
 
+async function collect(stream: AsyncIterable<Uint8Array>) {
+    const reader = stream[Symbol.asyncIterator]();
+    const chunks: Uint8Array[] = [];
+    while (true) {
+        const { done, value } = await reader.next();
+        if (done) {
+            return chunks;
+        }
+        chunks.push(value);
+    }
+};
+
+function joinBytes(...byteArrays: Uint8Array[]) {
+    const totalLength = byteArrays.reduce((acc, b) => acc + b.byteLength, 0);
+    const bytes = new Uint8Array(totalLength);
+    let offset = 0;
+    for (const array of byteArrays) {
+        bytes.set(array, offset);
+        offset += array.byteLength;
+    }
+    return bytes;
+}
+
+async function collectBytes(stream: AsyncIterable<Uint8Array>) {
+    const parts = await collect(stream);
+    return joinBytes(...parts);
+}
 function iterableToStream<T>(it: Iterable<T>) {
     return new ReadableStream({
         start(controller) {
@@ -24,6 +51,43 @@ function byteStringToStream(str: string, bufferSize: number = 5) {
     return iterableToStream(it);
 }
 
+function stripWhitespace(s: string) {
+    return s.replaceAll(/\s/g,"");
+}
+
+function concat(templateStringsArray: TemplateStringsArray, ...expr: (Uint8Array | string)[]) {
+    const strings = [...templateStringsArray];
+    let newString = "";
+    while (expr.length > 0) {
+        const s = strings.shift() || "";
+        const e = expr.shift();
+        newString += s;
+        if (e instanceof Uint8Array) {
+            newString += String.fromCharCode(...e);
+        } else if (typeof e === "string") {
+            newString += e;
+        }
+    }
+    const result = newString+strings.join("");
+    return result;
+}
+
+function hex(input: TemplateStringsArray, ..._: unknown[]) {
+    let newString = "";
+    const hex = stripWhitespace(input.join());
+    for (let i=0; i < hex.length; i+=2) {
+        const hexByte = hex.substring(i,i+2).padEnd(2,"0");
+        const byte = parseInt(hexByte,16);
+        newString += String.fromCharCode(byte);
+    }
+    return newString;
+}
+
+function b(input: TemplateStringsArray, ..._: unknown[]){
+    const byte = parseInt(stripWhitespace(input.join("")),2);
+    return String.fromCharCode(byte);
+}
+
 async function assertNext<T>(iterator: AsyncIterableIterator<T>): Promise<T> {
     const { value, done } = await iterator.next();
     assertEquals(done, false, "Done should be false");
@@ -34,8 +98,9 @@ async function literalTest(bytes: string, value: unknown, majorType: number) {
     const decoder = decoderFromStream(byteStringToStream(bytes));
     const iterator = decoder[Symbol.asyncIterator]();
     const next = await assertNext(iterator);
+    assertEquals(next.eventType, "literal", "Expect literal event");
     assertEquals(next.majorType, majorType, "Expect correct major type");
-    assertEquals(next.data, value, "Expect correct value");
+    assertEquals((next as LiteralEvent).data, value, "Expect correct value");
 }
 
 Deno.test(async function oneBytePositiveIntTest() {
@@ -46,19 +111,29 @@ Deno.test(async function oneBytePositiveIntTest() {
 });
 
 Deno.test(async function oneByteNegativeTest() {
-    await literalTest(`\x20`,-1,MajorType.NegativeInteger);
-    await literalTest(`\x2C`,-13,MajorType.NegativeInteger);
-    await literalTest(`\x37`,-24,MajorType.NegativeInteger);
+    await literalTest(hex`20`,-1,MajorType.NegativeInteger);
+    await literalTest(hex`2C`,-13,MajorType.NegativeInteger);
+    await literalTest(hex`37`,-24,MajorType.NegativeInteger);
 });
 
 Deno.test(async function numberTest() {
-    await literalTest(`\x18${''}\x7B`,123,MajorType.UnsignedInteger);
-    await literalTest(`\x38${''}\x7A`,-123,MajorType.NegativeInteger);
-    await literalTest(`\x39${''}\x30\x38`,-12345,MajorType.NegativeInteger);
-    await literalTest(`\x19${''}\x30\x39`,12345,MajorType.UnsignedInteger);
-    await literalTest(`\x39${''}\x30\x38`,-12345,MajorType.NegativeInteger);
-    await literalTest(`\x1A${''}\x00\xFF\xE8\xA2`,16771234,MajorType.UnsignedInteger);
-    await literalTest(`\x3A${''}\x00\xFF\xE8\xA1`,-16771234,MajorType.NegativeInteger);
-    await literalTest(`\x1B${''}\x01\xB6\x9B\x4B\xAC\xD0\x5F\x15`,123456789123456789n,MajorType.UnsignedInteger);
-    await literalTest(`\x3B${''}\x01\xB6\x9B\x4B\xAC\xD0\x5F\x14`,-123456789123456789n,MajorType.NegativeInteger);
+    await literalTest(hex`18 7B`,123,MajorType.UnsignedInteger);
+    await literalTest(hex`38 7A`,-123,MajorType.NegativeInteger);
+    await literalTest(hex`19 3039`,12345,MajorType.UnsignedInteger);
+    await literalTest(hex`39 3038`,-12345,MajorType.NegativeInteger);
+    await literalTest(hex`1A 00FFE8A2`,16771234,MajorType.UnsignedInteger);
+    await literalTest(hex`3A 00FFE8A1`,-16771234,MajorType.NegativeInteger);
+    await literalTest(hex`1B 01B69B4BACD05F15`,123456789123456789n,MajorType.UnsignedInteger);
+    await literalTest(hex`3B 01B69B4BACD05F14`,-123456789123456789n,MajorType.NegativeInteger);
+});
+
+Deno.test(async function byteString() {
+    const byteArray = new Uint8Array([1,2,3,4,5]);
+    const decoder = decoderFromStream(byteStringToStream(concat`${b`010 00101`}${byteArray}`));
+    const iterator = decoder[Symbol.asyncIterator]();
+    const next = await assertNext(iterator);
+    assertEquals(next.eventType, "start", "Expect start event");
+    assertEquals(next.majorType, MajorType.ByteString, "Expect correct major type");
+    const bytes = await collectBytes(consumeByteString(iterator));
+    assertEquals(bytes, byteArray, "Expect correct value");
 });
