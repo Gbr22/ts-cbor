e5458a3612ac9f487eb58da364a49b089d625f4b
Gabor Krisko <git@krisko.dev>
2025-01-25 22:40:12 +0100

    create IterationControl class

---
+++ iteration-control.ts
@@ -0,0 +1,45 @@
+type IterationControlType = "yield" | "return" | "continue";
+export class IterationControl<Yield, Exit> {
+    type: IterationControlType;
+    value: Yield | Exit | undefined
+
+    private constructor(type: IterationControlType, value?: Yield | Exit | undefined) {
+        this.type = type;
+        this.value = value;
+    }
+
+    static yield<Yield, Exit>(value: Yield): never {
+        throw new IterationControl<Yield, Exit>("yield", value);
+    }
+    static return<Yeild, Exit>(): never
+    static return<Yeild, Exit>(value: Exit): never
+    static return<Yeild, Exit>(value?: Exit | undefined): never {
+        throw new IterationControl<Yeild, Exit>("return",value);
+    }
+    static continue<Yeild, Exit>(): never {
+        throw new IterationControl<Yeild, Exit>("continue");
+    }
+
+    static createIterator<Yield, Return = void>(iterate: ()=>Promise<void>): AsyncIterableIterator<Yield,Return,void> {
+         async function* generator(): AsyncIterableIterator<Yield,Return,void> {
+            while (true) {
+                try {
+                    await iterate();
+                } catch(result) {
+                    if (result instanceof IterationControl) {
+                        if (result.type === "yield") {
+                            yield result.value;
+                        }
+                        if (result.type === "return") {
+                            return result.value;
+                        }
+                        continue;
+                    }
+                    throw result;
+                }
+            }
+        };
+
+        return generator();
+    }
+};
--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+import { assertExists } from "@std/assert/exists";
+import { IterationControl } from "./iteration-control.ts";
+
 export const MajorType = Object.freeze({
 	UnsignedInteger: 0,
 	NegativeInteger: 1,
@@ -70,9 +73,8 @@ export async function* consumeByteString(iterator: AsyncIterableIterator<CborEve
     }
 }
 
-const noYield = Symbol();
-
 type ReaderState = {
+	reader: ReadableStreamDefaultReader<Uint8Array>
 	currentBuffer: Uint8Array
 	mode: number
 	index: number
@@ -82,11 +84,11 @@ type ReaderState = {
 	numberValue: number | bigint
 	isIndefinite: boolean
 	byteArrayNumberOfBytesToRead: number
-	yieldValue: CborEvent | typeof noYield
 };
 
-function createReaderState(): ReaderState {
+function createReaderState(reader: ReadableStreamDefaultReader<Uint8Array>): ReaderState {
 	return {
+		reader,
 		currentBuffer: new Uint8Array(),
 		mode: Mode.ExpectingDataItem,
 		index: 0,
@@ -96,126 +98,124 @@ function createReaderState(): ReaderState {
 		numberValue: 0,
 		isIndefinite: false,
 		byteArrayNumberOfBytesToRead: 0,
-		yieldValue: noYield,
 	}
+}
+
+function flushNumber(state: ReaderState) {
+	if (isNumerical(state.majorType)) {
+		state.mode = Mode.ExpectingDataItem;
+		if (state.majorType === MajorType.NegativeInteger) {
+			if (typeof state.numberValue === "bigint") {
+				state.numberValue = (state.numberValue * -1n) -1n;
+			} else {
+				state.numberValue = (state.numberValue * -1) -1;
+			}
+		}
+		IterationControl.yield({
+			eventType: "literal",
+			majorType: state.majorType as any,
+			data: state.numberValue,
+		});
+	}
+	if (state.majorType == MajorType.ByteString) {
+		state.byteArrayNumberOfBytesToRead = Number(state.numberValue);
+		if (state.numberValue > Number.MAX_SAFE_INTEGER) {
+			throw new Error("Array too large");
+		}
+		IterationControl.yield({
+			eventType: "start",
+			majorType: MajorType.ByteString,
+		});
+	}
+}
+
+async function handleDecoderIterationData(state: ReaderState) {
+	if (state.mode == Mode.MajorType && state.majorType == MajorType.ByteString) {
+		if (state.byteArrayNumberOfBytesToRead <= 0) {
+			IterationControl.yield({
+				eventType: "end",
+				majorType: MajorType.ByteString,
+			});
+		}
+		const to = state.index + state.byteArrayNumberOfBytesToRead;
+		const slice = state.currentBuffer.slice(state.index, to);
+		state.index += state.byteArrayNumberOfBytesToRead;
+		IterationControl.yield({
+			eventType: "data",
+			majorType: MajorType.ByteString,
+			data: slice,
+		});
+	}
+	const byte = state.currentBuffer[state.index];
+	state.index++;
+	if (state.mode == Mode.ExpectingDataItem) {
+		state.majorType = byte >>> 5;
+		state.mode = Mode.MajorType;
+		state.additionalInfo = byte & 0b00011111;
+		state.numberValue = 0;
+		if (state.additionalInfo < 24) {
+			state.numberOfBytesToRead = 0;
+			state.numberValue = state.additionalInfo;
+			flushNumber(state);
+		}
+		if (state.additionalInfo == 24) {
+			state.numberOfBytesToRead = 1;
+		}
+		if (state.additionalInfo == 25) {
+			state.numberOfBytesToRead = 2;
+		}
+		if (state.additionalInfo == 26) {
+			state.numberOfBytesToRead = 4;
+		}
+		if (state.additionalInfo == 27) {
+			state.numberValue = 0n;
+			state.numberOfBytesToRead = 8;
+		}
+		if ([28,29,30].includes(state.additionalInfo)) {
+			throw new Error("Reserved");
+		}
+		if (state.additionalInfo == 31) {
+			state.isIndefinite = true;
+			state.numberOfBytesToRead = 0;
+		}
+		if (state.isIndefinite && isNumerical(state.majorType)) {
+			throw new Error(`Ill-formed Data Item of Major Type ${state.majorType}`);
+		}
+	} else if (state.mode == Mode.MajorType && state.numberOfBytesToRead > 0) {
+		if (typeof state.numberValue == "bigint") {
+			state.numberValue = (state.numberValue << 8n) | BigInt(byte);
+		} else {
+			state.numberValue = (state.numberValue << 8) | byte;
+		}
+		
+		state.numberOfBytesToRead--;
+		if (state.numberOfBytesToRead == 0) {
+			flushNumber(state);
+		}
+	}
+}
+
+async function handleDecoderIteration(state: ReaderState) {
+	if (state.index >= state.currentBuffer.length) {
+		const { done, value } = await state.reader.read();
+		if (done) {
+			IterationControl.return();
+		}
+		state.currentBuffer = value;
+		state.index = 0;
+	}
+	await handleDecoderIterationData(state);
 }
 
 export function decoderFromStream(stream: ReadableStream<Uint8Array>) {
 	const reader = stream.getReader();
-	const state = createReaderState();
+	const state = createReaderState(reader);
 
 	return {
 		[Symbol.asyncIterator]() {
-			return async function* it(): AsyncIterableIterator<CborEvent,void,void> {
-				function flushNumber() {
-					if (isNumerical(state.majorType)) {
-						state.mode = Mode.ExpectingDataItem;
-						if (state.majorType === MajorType.NegativeInteger) {
-							if (typeof state.numberValue === "bigint") {
-								state.numberValue = (state.numberValue * -1n) -1n;
-							} else {
-								state.numberValue = (state.numberValue * -1) -1;
-							}
-						}
-						state.yieldValue = {
-							eventType: "literal",
-							majorType: state.majorType as any,
-							data: state.numberValue,
-						};
-					}
-					if (state.majorType == MajorType.ByteString) {
-						state.byteArrayNumberOfBytesToRead = Number(state.numberValue);
-						if (state.numberValue > Number.MAX_SAFE_INTEGER) {
-							throw new Error("Array too large");
-						}
-						state.yieldValue = {
-							eventType: "start",
-							majorType: MajorType.ByteString,
-						};
-					}
-				}
-
-				while (true) {
-					if (state.yieldValue !== noYield) {
-						yield state.yieldValue;
-						state.yieldValue = noYield;
-					}
-					if (state.index >= state.currentBuffer.length) {
-						const { done, value } = await reader.read();
-						if (done) {
-							return;
-						}
-						state.currentBuffer = value;
-						state.index = 0;
-					}
-					if (state.mode == Mode.MajorType && state.majorType == MajorType.ByteString) {
-						if (state.byteArrayNumberOfBytesToRead <= 0) {
-							state.yieldValue = {
-								eventType: "end",
-								majorType: MajorType.ByteString,
-							}
-							continue;
-						}
-						const to = state.index + state.byteArrayNumberOfBytesToRead;
-						const slice = state.currentBuffer.slice(state.index, to);
-						state.index += state.byteArrayNumberOfBytesToRead;
-						state.yieldValue = {
-							eventType: "data",
-							majorType: MajorType.ByteString,
-							data: slice,
-						}
-						continue;
-					}
-					const byte = state.currentBuffer[state.index];
-					if (state.mode == Mode.ExpectingDataItem) {
-						state.majorType = byte >>> 5;
-						state.mode = Mode.MajorType;
-						state.additionalInfo = byte & 0b00011111;
-						state.numberValue = 0;
-						if (state.additionalInfo < 24) {
-							state.numberOfBytesToRead = 0;
-							state.numberValue = state.additionalInfo;
-							flushNumber();
-						}
-						if (state.additionalInfo == 24) {
-							state.numberOfBytesToRead = 1;
-						}
-						if (state.additionalInfo == 25) {
-							state.numberOfBytesToRead = 2;
-						}
-						if (state.additionalInfo == 26) {
-							state.numberOfBytesToRead = 4;
-						}
-						if (state.additionalInfo == 27) {
-							state.numberValue = 0n;
-							state.numberOfBytesToRead = 8;
-						}
-						if ([28,29,30].includes(state.additionalInfo)) {
-							throw new Error("Reserved");
-						}
-						if (state.additionalInfo == 31) {
-							state.isIndefinite = true;
-							state.numberOfBytesToRead = 0;
-						}
-						if (state.isIndefinite && isNumerical(state.majorType)) {
-							throw new Error(`Ill-formed Data Item of Major Type ${state.majorType}`);
-						}
-					} else if (state.mode == Mode.MajorType && state.numberOfBytesToRead > 0) {
-						if (typeof state.numberValue == "bigint") {
-							state.numberValue = (state.numberValue << 8n) | BigInt(byte);
-						} else {
-							state.numberValue = (state.numberValue << 8) | byte;
-						}
-						
-						state.numberOfBytesToRead--;
-						if (state.numberOfBytesToRead == 0) {
-							flushNumber();
-						}
-					}
-
-					state.index++;
-				}
-			}();
+			return IterationControl.createIterator<CborEvent>(async () => {
+				await handleDecoderIteration(state);
+			});
 		}
 	}
 }
